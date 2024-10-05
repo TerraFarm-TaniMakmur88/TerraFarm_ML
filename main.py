85e40072b0c8ba0e7fd459c62ddf65c60008bdba
Muhammad Fadhil Amri <fadhil133113@gmail.com>
2024-10-05 21:41:09 +0700

    feat: add forecast windspeed API

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from service.forecast import forecast_temperature_service
+from service.forecast import forecast_temperature_service, forecast_windspeed_service
 # from model.schemas import TemperatureForecastInputModel
 
 app = FastAPI()
@@ -13,5 +13,13 @@ async def forecast_temperature(year:int, month:int, day:int, hour:int, lat:float
     try:
         temperature_forecasted = await forecast_temperature_service(year, month, day, hour, lat, long, windspeed, humidity, precipitation)
         return {"status": "OK", "data": float(temperature_forecasted[0])}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@app.get("/forecast/windspeed")
+async def forecast_windspeed(year:int, month:int, day:int, hour:int, lat:float, long:float, temperature:float, humidity:float, precipitation:float):
+    try:
+        windspeed_forecasted = await forecast_windspeed_service(year, month, day, hour, lat, long, temperature, humidity, precipitation)
+        return {"status": "OK", "data": float(windspeed_forecasted[0])}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- service/forecast.py
+++ service/forecast.py
@@ -7,6 +7,11 @@ import numpy as np
 async def forecast_temperature_service(year:int, month:int, day:int, hour:int, lat:float, long:float, windspeed:float, humidity:float, precipitation:float):
     print("kepanggil",year, month, day, hour, windspeed, humidity, precipitation,lat, long)
     input_data = np.array([[year, month, day, hour, windspeed, humidity, precipitation,lat, long]])
-    # data_sample =  np.array([[2024, 10, 7, 13, 31.0 , 49.0, 0, -6.914744, 107.609810]]) # bandung
     forecasted_temperature = temperature_model.predict(input_data)
     return forecasted_temperature
+
+async def forecast_windspeed_service(year:int, month:int, day:int, hour:int, lat:float, long:float, temperature:float, humidity:float, precipitation:float):
+    print("kepanggil",year, month, day, hour, temperature, humidity, precipitation,lat, long)
+    input_data = np.array([[year, month, day, hour, temperature, humidity, precipitation,lat, long]])
+    forecasted_windspeed = windspeed_model.predict(input_data)
+    return forecasted_windspeed
